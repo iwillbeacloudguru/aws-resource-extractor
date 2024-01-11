9db935e13703cdda9f960f8a02880aed81b8faf3
huayong <porames.jari@gmail.com>
2024-01-11 23:26:20 +0700

    Add multi-accounts features

--- README.md
+++ README.md
@@ -16,30 +16,53 @@ Before using this application, ensure that you have the following prerequisites 
     https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
 - Git
     https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+- AWS Credential must have access to Organization Read-only
 
 ## Getting Started
 1. Clone this git repo to your local machine.
     ``` bash
     git clone https://github.com/iwillbeacloudguru/aws-resource-extractor.git
     ```
+    <br />
 2. Change directory to project folder.
     ``` bash
     cd aws-resource-extractor
     ```
+    <br />
 3. Create and activate virtual environment.
     ``` Python
     virtualenv -p python3.9 .env && source .env/bin/activate
     ```
-3. Install required packages in virtual environment.
+    <br />
+4. Install required packages in virtual environment.
     ``` Python
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
     ```
-4. Configure AWS CLI
+    <br />
+5. Configure AWS CLI
     https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html
+    <br />
 
-5. Set up the AWS CLI
+6. Set up the AWS CLI
     https://docs.aws.amazon.com/cli/latest/userguide/getting-started-quickstart.html
     <i>Note: Please note your AWS profile in cli that you want to use.</i>
+    <br />
+
+7. Turn on Resource Explorer. <b>Repeat this step in every account.</b></br >
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.42.23@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.43.55@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.47.50@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.50.05@2x.png>)
+    <br />
+8. Create IAM Role for script to assume. <b>Repeat this step in every account.</b></br >
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.51.49@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.52.57@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.53.48@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 19.57.43@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 20.03.07@2x.png>)
+    ![Alt text](<assets/CleanShot 2567-01-11 at 20.04.35@2x.png>)
+    <br />
+
 
 ## Usage
 
@@ -47,20 +70,20 @@ Before using this application, ensure that you have the following prerequisites 
 To start using the application, run the following command:
 
 ``` bash
-python main.py init
+python3 main.py init
 ```
 Follow the prompts to select an AWS profile and set up your configuration.
 
 ### Querying Resources
 To query AWS resources, run the following command:
 ``` bash
-python main.py query
+python3 main.py query
 ```
 ## Demo
-![Alt text](<CleanShot 2567-01-08 at 12.02.08.gif>)
+![Alt text](<assets/CleanShot 2567-01-08 at 12.02.08.gif>)
 ## Saving Results
 You can choose to save the results as a CSV file during the query process. The CSV files will be named with a timestamp.
-![Alt text](<CleanShot 2567-01-08 at 12.26.02@2x.png>)
+![Alt text](<assets/CleanShot 2567-01-08 at 12.26.02@2x.png>)
 ## Configuration
 You can modify the configuration in the setup.json file to customize your settings.
 
--- main.py
+++ main.py
@@ -4,16 +4,24 @@ from examples import custom_style_2
 import boto3
 import botocore
 import click
-from tqdm import tqdm
-import time
+# import time
+from datetime import datetime
 import json
 import pandas as pd
 
-def start_session(AWSProfile):
+def start_session(AWSProfile, account):
     try:
         boto3.setup_default_session(profile_name=AWSProfile)
-        client = boto3.client('resource-explorer-2')
-        return client
+        sts_client = boto3.client('sts')
+        response = sts_client.assume_role(
+            RoleArn="arn:aws:iam::" + account +":role/ResourceExplorerRole",
+            RoleSessionName="AssumeRoleSession1"
+        )
+        new_session = boto3.Session(aws_access_key_id=response['Credentials']['AccessKeyId'],
+                            aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+                            aws_session_token=response['Credentials']['SessionToken'],
+                            region_name="ap-southeast-1")
+        return new_session
     except botocore.exceptions.ClientError as e:
         if e in "botocore.exceptions.EndpointConnectionError":
             print("Please check your profile configuration is correct.")
@@ -23,9 +31,45 @@ def start_session(AWSProfile):
             print("- CLI profile name")
         else:
             raise
+        
+    # try:
+    #     boto3.setup_default_session(profile_name=AWSProfile)
+    #     client = boto3.client('organizations')
+    #     response = client.list_accounts()
+    #     return response['Accounts']
+    # except botocore.exceptions.ClientError as e:
+    #     raise
 
-def close_session():
-    pass
+def list_org_accounts(AWSProfile):
+    boto3.setup_default_session(profile_name=AWSProfile)
+    org_client = boto3.client('organizations')
+    accounts_response = org_client.list_accounts()
+    accounts = []
+    account_choices = [
+            Separator("===== Either choose ALL or manually select accounts ======"),
+            {
+                'name': 'ALL'
+            },
+            Separator("===== List of all accounts available ======"),
+        ]
+    for account in accounts_response['Accounts']:
+        accounts += [account['Id']]
+        account_choices += [{'name': account['Id']}]
+    return {"account_choices": account_choices, "accounts": accounts}
+
+# def list_view(account):
+#     client = start_session("", account)
+#     try:
+#         return client.list_views()
+#     except botocore.exceptions.ClientError as e:
+#         raise
+
+def create_view(account):
+    client = start_session("", account)
+    try:
+        return client.create_view(ViewName="all-resources")['View']['ViewArn']
+    except botocore.exceptions.ClientError as e:
+        raise
 
 @click.group()
 def main():
@@ -33,59 +77,58 @@ def main():
 
 @main.command()
 def init():
+    view_arn = []
     questions = [{'type': 'list', 'name': 'AWSProfile', 'message': 'Which AWS profile do you want to use?: ', 'choices': [str(profile) for profile in boto3.session.Session().available_profiles]}]
     AWSProfile = prompt(questions)['AWSProfile']
     try:
-        client = start_session(AWSProfile)
+        tmp = list_org_accounts(AWSProfile)
+        account_choices, accounts = tmp['account_choices'], tmp['accounts']
+        org_questions = [{'type': 'checkbox', 'name': 'organization', 'message': 'Select which account in organization to query: ', 'choices': account_choices,'validate': lambda answer: 'You must choose at least one resource.' if len(answer) == 0 else True, 'paginate': True, 'pageSize': 100}]
+        for account in accounts:
+            re2_client = start_session(AWSProfile, account).client('resource-explorer-2')
+            response = re2_client.list_views()
+            if response['Views'] == []:
+                print("\nNo view presents.\n")
+                try:
+                    questions = [{'type': 'confirm', 'name': 'createViews?', 'message': 'Do you want to create new views?: ', 'default': False}]
+                    answers = prompt(questions)
+                    if answers['createViews?'] == False:
+                        exit()
+                    else:
+                        view_arn += [create_view(AWSProfile)]
+                except botocore.exceptions.ClientError as e:
+                    raise
+            else:
+                print("\nView(s) already exist in account " + account + ".")
+                try:
+                    questions = [{'type': 'list', 'name': 'selectViews', 'message': 'Select which view you want to use to query: ', 'choices': [str(view) for view in response['Views']]}]
+                    answers = prompt(questions)
+                    view_arn += [answers['selectViews']]
+                except botocore.exceptions.ClientError as e:
+                    if e in "botocore.exceptions.ClientError: An error occurred (AccessDenied) when calling the AssumeRole operation:":
+                        print("Please check that IAM role trusted relationship and role name are valid.")
+                    else:
+                        raise
     except botocore.exceptions.ClientError as e:
         if e in "botocore.exceptions.TokenRetrievalError":
             print("Please check/reconfigure AWS CLI credentails.")
             print("    - " + AWSProfile)
         else:
             raise
-    response = list_view(AWSProfile)
-    if response['Views'] == []:
-        print("\nNo view presents.\n")
-        try:
-            questions = [{'type': 'confirm', 'name': 'createViews?', 'message': 'Do you want to create new views?: ', 'default': False}]
-            answers = prompt(questions)
-            if answers['createViews?'] == False:
-                exit()
-            else:
-                view_arn = create_view(AWSProfile)
-        except botocore.exceptions.ClientError as e:
-            raise
-    else:
-        print("\nView(s) already exist.")
-        # for view in [str(view) for view in response['Views']]:
-        #     print("-", view)
-        try:
-            questions = [{'type': 'list', 'name': 'selectViews', 'message': 'Select which view you want to use to query: ', 'choices': [str(view) for view in response['Views']]}]
-            answers = prompt(questions)
-            view_arn = answers['selectViews']
-        except botocore.exceptions.ClientError as e:
-            raise
+    print("\n")
     questions = [{'type': 'confirm', 'name': 'saveCSV?', 'message': 'Do you want to create CSV file for output?: ', 'default': False}]
-    answers = prompt(questions)
-    save_lock(view_arn, AWSProfile, answers['saveCSV?'])
+    csv_answers = prompt(questions)
+    print("\n")
+    org_answers = prompt(org_questions)
+    if 'ALL' in org_answers['organization']:
+        save_lock(view_arn, AWSProfile, accounts, csv_answers['saveCSV?'])
+    else:
+        save_lock(view_arn, AWSProfile, org_answers['organization'], csv_answers['saveCSV?'])
     print("\nPlease use following command to query:")
     print("    python main.py query\n")
-            
-def list_view(AWSProfile):
-    print("Starting session: ")
-    client = start_session(AWSProfile)
-    # start_time = time.time()
-    try:
-        return client.list_views()
-    except botocore.exceptions.ClientError as e:
-        raise
-    
-def create_view(AWSProfile):
-    client = start_session(AWSProfile)
-    return client.create_view(ViewName="all-resources")['View']['ViewArn']
 
-def save_lock(view_arn, AWSProfile, csv):
-    dictionary = {"create_date": time.time(), "AWSProfile": AWSProfile, "view_arn": view_arn}
+def save_lock(view_arn, AWSProfile, accounts, csv):
+    dictionary = {"create_date": str(datetime.now()), "AWSProfile": AWSProfile, "view_arn": view_arn, "accounts": accounts}
     if csv == True:
         dictionary['saveCSV'] = True
     else:
@@ -96,6 +139,7 @@ def save_lock(view_arn, AWSProfile, csv):
 
 @main.command()
 def query():
+    total = pd.DataFrame(columns = ['Arn', 'LastReportedAt', 'OwningAccountId', 'Properties', 'Region', 'ResourceType', 'Service'])
     try:
         with open('setup.json') as f:
             jd = json.load(f)
@@ -103,9 +147,8 @@ def query():
         print("No setup file found.\nPlease run following command to start:\n    python main.py query\n")
         raise
     AWSProfile = jd['AWSProfile']
-    client = start_session(AWSProfile)
+    # client = start_session(AWSProfile)
     try:
-        # print(new_dataframe.head())
         choices = [
             Separator("===== Either choose ALL or manually select services ======"),
             {
@@ -113,61 +156,55 @@ def query():
             },
             Separator("===== List of all services available ======"),
         ]
-        # with open('services.json') as f:
-        #     jd = json.load(f)
-        # service_df = pd.json_normalize(jd, record_path=['Services'])
         session = boto3.Session(profile_name=AWSProfile)
         services = session.get_available_services()
-        # for service in service_df.ServiceCode:
         for service in services:
             choices += [{'name': service}]
         questions = [{'type': 'checkbox', 'name': 'resourcesQuery?', 'message': 'Which resources do you want to query?: ', 'choices': choices, 'validate': lambda answer: 'You must choose at least one resource.' if len(answer) == 0 else True, 'paginate': True, 'pageSize': 100}]
         answers = prompt(questions)
         with open('setup.json', 'r') as openfile:
             view_arn = json.load(openfile)
-        # print(answers['resourcesQuery?'])
         if "ALL" in answers['resourcesQuery?']:
-            # print(choices)
-            # resource_query(service_df.ServiceCode, view_arn)
-            resource_query(services, view_arn, AWSProfile)
+            for account in jd['accounts']:
+                print("\n\nStart account " + account + " query.\n")
+                tmp = resource_query(services, view_arn, AWSProfile, account)
+                total = pd.concat([total, tmp], ignore_index=True, verify_integrity=True, axis=0)
+                print("====================================")
+            total = total.drop_duplicates(subset=['Arn'], keep='last')
+            # print(total)
+            save_to_csv(datetime.now(), total)
         else:
-            # resource_query(answers['resourcesQuery?'], view_arn)
-            pass
+            exit()
     except botocore.exceptions.ClientError as e:
         raise
     exit()
     
-def resource_query(resource_list, view_arn, AWSProfile):
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-    client = start_session(AWSProfile)
+def resource_query(resource_list, view_arn, AWSProfile, account):
+    # timestamp = time.strftime("%Y%m%d-%H%M%S")
     tmp = pd.DataFrame(columns = ['Arn', 'LastReportedAt', 'OwningAccountId', 'Properties', 'Region', 'ResourceType', 'Service'])
+    re2_client = start_session(AWSProfile, account).client('resource-explorer-2')
+    view = ""
+    for arn in view_arn['view_arn']:
+        if arn.startswith("arn:aws:resource-explorer-2:ap-southeast-1:" + str(account) + ":view/all-resources/"):
+            view = arn
+        else:
+            pass
     for resource in resource_list:
-        response = client.search(QueryString=resource, ViewArn=view_arn['view_arn'])
+        response = re2_client.search(QueryString=resource, ViewArn=view)
         try:
             if response['Resources'] == []:
                 pass
             else:
                 items = pd.DataFrame(response['Resources'], columns = ['Arn', 'LastReportedAt', 'OwningAccountId', 'Properties', 'Region', 'ResourceType', 'Service'])
-                # resource = pd.DataFrame(columns = ['Arn', 'LastReportedAt', 'OwningAccountId', 'Properties', 'Region', 'ResourceType', 'Service'])
-                # print("================================")
-                # print(items.dtypes)
-                # print(resource.dtypes)
-                # print("================================")
-                # resource = resource.append(resource, ignore_index = True)
-                tmp = pd.concat([tmp, items], ignore_index=True)
-                # print(resource)
-                # resource = pd.DataFrame(response['Resources'], columns = ['Arn', 'LastReportedAt', 'OwningAccountId', 'Properties', 'Region', 'ResourceType', 'Service'])
-                # tmp = pd.concat([resource if not resource.empty else None, tmp], verify_integrity=True, ignore_index = True)
+                tmp = pd.concat([tmp, items], ignore_index=True, verify_integrity=True, axis=0)
         except ValueError as e:
             print('Could not find resource.')
-        # tmp = tmp.drop_duplicates(subset=['Arn'], keep='last')
         print(resource + ": Completed")
-        print("\n")
-    tmp = tmp.drop_duplicates(subset=['Arn'], keep='last')
-    save_to_csv(timestamp, tmp)
+    return tmp
+    # save_to_csv(timestamp, tmp)
                 
 def save_to_csv(timestamp, df):
-    df.to_csv(timestamp + '.csv', mode='a', index=False, header=True)
+    df.to_csv(str(timestamp) + '.csv', mode='a', index=False, header=True)
 
 def exit():
     f = open('bye.txt', 'r')
